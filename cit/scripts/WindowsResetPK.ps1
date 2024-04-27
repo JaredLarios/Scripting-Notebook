267eaed984ec94cafdbe16cea00e27a00d33c872
JaredLarios <lar21038@byui.edu>
2024-04-27 15:59:27 -0600

    fix: reset and install pk issue of not running code solved.

--- cit/scripts/WindowsResetPK.ps1
+++ cit/scripts/WindowsResetPK.ps1
@@ -1,16 +1,20 @@
 Param(
-    [string]$PK
+    [string]$PK,
+    [string]$RemovePK = "y"
 )
 
 nslookup -type=srv _vlmcs._tcp >%temp%\kms.txt
+If (-Not $RemovePK -eq 'n') {
+    cscript /NoLogo "c:\Windows\system32\slmgr.vbs" /upk
+    Write-Host "[+] Product Key Removed"
+}
 If (-Not $PK -eq '' ) {
-    slmgr /ipk $($PK)
+    cscript /NoLogo "c:\Windows\system32\slmgr.vbs" /ipk $($PK)
     Write-Host "[+] Added Product Key $PK"
 }
 
 net stop sppsvc
-Write-Host "[+] Network Security Service Stoped"
 net start sppsvc
-Write-Host "[+] Network Security Service Started"
-slmgr /dli
+
+cscript /NoLogo "c:\Windows\system32\slmgr.vbs" /dli
 Write-Host "[+] Key Product status"
